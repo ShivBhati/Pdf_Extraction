0a0b9672a84d02468e1b3641ca5619689b21de46
ShivBhati <bhatishivam0007@gmail.com>
2023-11-28 12:12:28 +0530

    Extracting_extra_Pages

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -3,11 +3,12 @@ import openpyxl
 import pandas as pd
 import fitz
 from PyPDF2 import PdfReader
+from PyPDF2 import PdfWriter
+
+pdf_writer = PdfWriter()
 
 def extracting_pdf_pages(x,y,z):
-
-    exdir = os.listdir(x)
-    
+    exdir = os.listdir(x)    
     for e in exdir:
         exlfilename = os.path.join(x,e)
         with open(exlfilename, 'rb') as exl:
@@ -18,37 +19,31 @@ def extracting_pdf_pages(x,y,z):
 
 def openpdf(y,df,z):
     exdir = os.listdir(y)
-    for p in exdir:
-        input_pdf = os.path.join(y,p)
-        pdf_document = PdfReader(input_pdf)
-        pdf_num_pages = len(pdf_document.pages)
-        for index, row in df.iterrows():
-            startpage = ''
-            endpage = ''
+    for index, row in df.iterrows():
+        for p in exdir:
+            input_pdf = os.path.join(y,p)
+            pdf_document = PdfReader(input_pdf)
+            pdf_num_pages = len(pdf_document.pages)
             found = 'No'
-            for page_num in range(pdf_num_pages):
-                page = pdf_document.pages[page_num]
-                text = page.extract_text()
-                if row['Invoice_Number'] in text and 'Statement' not in text:
-                    if startpage == "": 
-                        startpage =  page
-                    else:
-                        endpage = page
-                    found = 'yes'
-                else:
-                    continue
-            output_document = fitz.open()
-            output_document.insert_pdf(pdf_document, from_page=startpage, to_page=endpage)
-            output_path = os.path.join(z,row['Invoice_Number'])    
-            output_document.save(output_path)
-            output_document.close()
-            if found == 'yes':
-                break
-            else:
-                continue
-        pdf_document.close()
+            for page_num in range(0, pdf_num_pages+1):
+                print(f'the pdf {p} contains {page_num} pages')
+            #     page = pdf_document.pages[page_num]
+            #     text = page.extract_text()
+            #     if row['Invoice_Number'] in text and 'PLEASE PAY FROM THIS STATEMENT' not in text:
+            #         pdf_writer.add_page(page)
+            #         found = 'yes'
+            #     else:
+            #         continue
+            # if found == 'yes':
+            #     output_path = os.path.join(z,row['Invoice_Number']+".pdf")    
+            #     with open(output_path,'wb') as output:
+            #         pdf_writer.write(output)
+            #     break
+            # else:
+            #     continue
+        
 
-def main():
+def main():  
     open_file = r'B:\Python\Git\Pdf_Extraction\excel'
     pdf_path = r'B:\Python\Git\Pdf_Extraction\pdf'
     extract_file_path = r'B:\Python\Git\Pdf_Extraction\extracted_pages'
